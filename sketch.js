e2154b307e5e992b142605dda700e166add58204
Vedant7220 <69467371+Vedant7220@users.noreply.github.com>
2020-09-21 18:57:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var gamestate = 0
+var playercount
+var database,form,player,game;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game = new Game();
+  game.getState();
+  game.start();
+
+ 
+}
+
+function draw(){
+  background("white");
+  
+    
+    drawSprites();
+  
+}
+
